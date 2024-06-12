c958ae3138a06e9001516e1cbee9d92528a11d44
Jack Duvall <10897431+duvallj@users.noreply.github.com>
2024-06-12 16:21:24 -0400

    manage neovim dotfiles with home-manager

--- nix/darwin/neovim.nix
+++ nix/darwin/neovim.nix
@@ -1,8 +1,18 @@
 { pkgs, ... }: {
-  home.packages = [ pkgs.neovim ];
   programs.neovim = {
+    enable = true;
     viAlias = true;
     vimAlias = true;
     vimdiffAlias = true;
+
+    # Doesn't work, since it needs to go at the end instead
+    # extraConfig = builtins.readFile ./dotfiles/.vimrc;
+    extraLuaConfig = builtins.readFile ./dotfiles/init.lua;
+  };
+
+  home.file = {
+    ".vim".source = ./dotfiles/.vim;
+    ".vimrc".source = ./dotfiles/.vimrc;
+    ".config/nvim/coc-settings.json".source = ./dotfiles/coc-settings.json;
   };
 }
