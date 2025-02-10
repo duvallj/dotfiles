65c727b4a81f0c1af3cbd0a14959a5565401bc36
Jack Duvall <10897431+duvallj@users.noreply.github.com>
2025-02-10 11:35:24 -0500

    update asdf

--- .extra-darwin-work.zsh
+++ .extra-darwin-work.zsh
@@ -7,10 +7,10 @@ fi
 eval "$(brew shellenv)"
 export PATH="$(brew --prefix)/opt/mysql-client@8.0/bin:${PATH}"
 
+export ASDF_DATA_DIR="${HOME}/.asdf"
+export PATH="${ASDF_DATA_DIR}/shims:${PATH}"
+export PATH="${HOME}/.local/bin:$(go env GOPATH)/bin:${PATH}"
+
 export WONDER_ROOT="${HOME}/wonder/src"
 export CROAM_ENV="local"
 source "${HOME}/wonder-aliases.sh"
-
-source "$(brew --prefix)/opt/asdf/libexec/asdf.sh"
-export PATH="${HOME}/.local/bin:$(go env GOPATH)/bin:${PATH}"
-
