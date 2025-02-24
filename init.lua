87e02aeeb95e0a37c853deb1bc55fe78ebf8b2db
Jack Duvall <10897431+duvallj@users.noreply.github.com>
2025-02-24 10:54:12 -0500

    move config inside lazy setup

--- init.lua
+++ init.lua
@@ -26,16 +26,39 @@ require('lazy').setup({
     branch = 'release',
   },
   {
-    'nvim-telescope/telescope-fzf-native.nvim',
-    branch = 'main',
-    build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release'
-  },
-  {
     'nvim-telescope/telescope.nvim',
     branch = '0.1.x',
     dependencies = {
       'nvim-lua/plenary.nvim'
-    }
+    },
+    opts = {
+      defaults = {
+        mappings = {
+          i = {
+            ["<CR>"] = "select_tab",
+          },
+          n = {
+            ["<CR>"] = "select_tab",
+          },
+        },
+      },
+      extensions = {
+        fzf = {
+          fuzzy = true,
+          override_generic_sorter = true,
+          override_file_sorter = true,
+          case_mode = "smart_case",
+        }
+      },
+    },
+  },
+  {
+    'nvim-telescope/telescope-fzf-native.nvim',
+    branch = 'main',
+    build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release',
+    config = function()
+      require('telescope').load_extension('fzf')
+    end,
   },
   {
     'tpope/vim-repeat',
@@ -56,6 +79,44 @@ require('lazy').setup({
   {
     'lewis6991/gitsigns.nvim',
     tag = 'v0.8.1',
+    opts = {
+      on_attach = function(bufnr)
+        local gitsigns = require('gitsigns')
+
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
+
+        -- Navigation
+        map('n', ']c', function()
+          if vim.wo.diff then
+            vim.cmd.normal({']c', bang = true})
+          else
+            gitsigns.nav_hunk('next')
+          end
+        end)
+
+        map('n', '[c', function()
+          if vim.wo.diff then
+            vim.cmd.normal({'[c', bang = true})
+          else
+            gitsigns.nav_hunk('prev')
+          end
+        end)
+
+        -- Actions
+        map('n', '<leader>hs', gitsigns.stage_hunk)
+        map('n', '<leader>hr', gitsigns.reset_hunk)
+        map('v', '<leader>hs', function() gitsigns.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+        map('v', '<leader>hr', function() gitsigns.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+        map('n', '<leader>hS', gitsigns.stage_buffer)
+        map('n', '<leader>hu', gitsigns.undo_stage_hunk)
+        map('n', '<leader>hR', gitsigns.reset_buffer)
+        map('n', '<leader>hp', gitsigns.preview_hunk)
+      end
+    }
   },
   {
     'rbong/vim-flog',
@@ -78,68 +139,6 @@ require('lazy').setup({
     end,
   },
 })
-
-require('telescope').setup {
-  defaults = {
-    mappings = {
-      i = {
-        ["<CR>"] = "select_tab",
-      },
-      n = {
-        ["<CR>"] = "select_tab",
-      },
-    },
-  },
-  extensions = {
-    fzf = {
-      fuzzy = true,
-      override_generic_sorter = true,
-      override_file_sorter = true,
-      case_mode = "smart_case",
-    }
-  }
-}
-
-require('telescope').load_extension('fzf')
-
-require('gitsigns').setup {
-  on_attach = function(bufnr)
-    local gitsigns = require('gitsigns')
-
-    local function map(mode, l, r, opts)
-      opts = opts or {}
-      opts.buffer = bufnr
-      vim.keymap.set(mode, l, r, opts)
-    end
-
-    -- Navigation
-    map('n', ']c', function()
-      if vim.wo.diff then
-        vim.cmd.normal({']c', bang = true})
-      else
-        gitsigns.nav_hunk('next')
-      end
-    end)
-
-    map('n', '[c', function()
-      if vim.wo.diff then
-        vim.cmd.normal({'[c', bang = true})
-      else
-        gitsigns.nav_hunk('prev')
-      end
-    end)
-
-    -- Actions
-    map('n', '<leader>hs', gitsigns.stage_hunk)
-    map('n', '<leader>hr', gitsigns.reset_hunk)
-    map('v', '<leader>hs', function() gitsigns.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
-    map('v', '<leader>hr', function() gitsigns.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
-    map('n', '<leader>hS', gitsigns.stage_buffer)
-    map('n', '<leader>hu', gitsigns.undo_stage_hunk)
-    map('n', '<leader>hR', gitsigns.reset_buffer)
-    map('n', '<leader>hp', gitsigns.preview_hunk)
-  end
-}
 
 -- Let kitty know when nvim is open
 vim.api.nvim_create_autocmd({ "VimEnter", "VimResume" }, {
