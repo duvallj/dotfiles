3518def1a6fa5528dcdc9b18a17914e492ec288f
Jack Duvall <10897431+duvallj@users.noreply.github.com>
2025-11-06 12:42:59 -0500

    Make clicking on file open it in existing Neovim window
    This was pretty tricky, but overall I think the result is worth it!
    Finally, I have something approaching the convenience of the normal
    VSCode workflow lol

--- .extra.zsh
+++ .extra.zsh
@@ -13,6 +13,32 @@ function replace {
   rg -l "$1" $3 | xargs sd "$1" "$2"
 }
 
+function nvim {
+  if command -v kitten &> /dev/null; then
+    # If running in Kitty, get the tab id for a remote Neovim instance
+    tab_id="$(kitten @ ls --match "id:${KITTY_WINDOW_ID}" | jq '.[0].tabs[0].id')"
+    pipe="${XDG_CACHE_HOME:-${HOME}/.cache}/nvim/server-${tab_id}.pipe"
+    if [ -S "${pipe}" ]; then
+      if [ "$1" = "-k" ] || [ "$1" = "--kitty-remote" ]; then
+        # Run command in existing instance
+        shift
+        command nvim --server "${pipe}" --remote-tab "$@"
+      else
+        # Launch new, non-remote instance
+        command nvim "$@"
+      fi
+    else
+      # Launch instance if there isn't one already
+      if [ "$1" = "-k" ] || [ "$1" = "--kitty-remote" ]; then
+        shift
+      fi
+      command nvim --listen "${pipe}" "$@"
+    fi
+  else
+    command nvim "$@"
+  fi
+}
+
 function xim {
   xargs nvim -p "$@"
 }
@@ -34,11 +60,19 @@ function extract {
 
 function pbim {
   # Extract all files from the 
-  pbpaste | extract | xim
+  pbpaste | extract | xim "$@"
 }
 
 function conflicts {
-  git conflicts | xim
+  git conflicts | xim "$@"
+}
+
+function unstaged {
+  git diff --name-only | xim "$@"
+}
+
+function staged {
+  git diff --name-only --staged | xim "$@"
 }
 
 function cam {
