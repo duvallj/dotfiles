35307aa4e86bfae7a2a7921a5f772478251d7538
Jack Duvall <10897431+duvallj@users.noreply.github.com>
2025-11-19 16:48:36 -0500

    make `prune` support git worktrees
    turns out it has a different symbol (+) for denoting those. Not too hard
    to add to the exclude pattern.

--- .extra-darwin.zsh
+++ .extra-darwin.zsh
@@ -1,6 +1,6 @@
 function prune {
   header="${1:-jack}"
-  git branch --list | rg -v "^\*|${header}" | xargs git branch -D
+  git branch --list | rg -v "^[*+]|${header}" | xargs git branch -D
   git remote prune origin | sed -n -E "/${header}/ s/.*(${header}.*)/\\1/p" | xargs git branch -D
 }
 
