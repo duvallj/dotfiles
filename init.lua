b62415903c722db66f5824e46c9b892d82f8523a
Jack Duvall <10897431+duvallj@users.noreply.github.com>
2026-03-03 20:34:06 -0500

    try to not have OilActionsPost time out
    ... by wrapping the timing-out function in a "schedule". tbh i haven't
    tested this yet, maybe it won't work but surely it'll be better than the
    status quo!

--- init.lua
+++ init.lua
@@ -763,7 +763,9 @@ vim.api.nvim_create_autocmd("User", {
   pattern = "OilActionsPost",
   callback = function(event)
     if event.data.actions[1].type == "move" then
-      Snacks.rename.on_rename_file(event.data.actions[1].src_url, event.data.actions[1].dest_url)
+      vim.schedule(function()
+        Snacks.rename.on_rename_file(event.data.actions[1].src_url, event.data.actions[1].dest_url)
+      end)
     end
   end,
 })
