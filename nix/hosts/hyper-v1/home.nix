0ade4f5e46ac60c1b9ab1894e1c9df9f35d0f329
Jack Duvall <10897431+duvallj@users.noreply.github.com>
2026-03-12 18:53:22 -0400

    remove neovim server aliases
    my kitty setup is more than sufficient now lol

--- nix/hosts/hyper-v1/home.nix
+++ nix/hosts/hyper-v1/home.nix
@@ -8,8 +8,6 @@
   home.stateVersion = "23.11";
   programs.home-manager.enable = true;
 
-  programs.neovim.serverAliases = true;
-
   programs.zsh.kittyExtra.enable = false;
   programs.zsh.initContent = builtins.readFile ../../../.zshrc.extra-hyper-v1;
 
--- nix/modules/home-manager/neovim.nix
+++ nix/modules/home-manager/neovim.nix
@@ -21,56 +21,37 @@ in
 {
   options.programs.neovim = {
     nonManagedEnable = lib.mkEnableOption "Whether to enable my custom non-managed Neovim configuration";
-    serverAliases = lib.mkOption {
-      default = false;
-      description = "Whether to enable RPC aliases";
-      type = lib.types.bool;
-    };
   };
 
-  config = lib.mkIf cfg.nonManagedEnable (
-    lib.mkMerge [
-      {
-        # I don't use this because I want to track these dotfiles directly from git instead.
-        # programs.neovim.enable = true;
+  config = lib.mkIf cfg.nonManagedEnable {
+    # I don't use this because I want to track these dotfiles directly from git instead.
+    # programs.neovim.enable = true;
 
-        home.sessionVariables = {
-          EDITOR = "nvim";
-        };
+    home.sessionVariables = {
+      EDITOR = "nvim";
+    };
 
-        home.shellAliases = {
-          vimdiff = "nvim -d";
-        };
+    home.shellAliases = {
+      vimdiff = "nvim -d";
+    };
 
-        home.packages = [
-          wrappedNeovim
-        ]
-        ++ (with pkgs; [
-          # For viewing images with Snacks.nvim
-          ffmpeg-full
-          ghostscript_headless
-          imagemagick
-          # For treesitter
-          tree-sitter
-          # TODO: add more configuration options for language servers
-          clang-tools
-          nixd
-          nixfmt-rfc-style
-          vscode-js-debug
-          vscode-langservers-extracted
-          (callPackage ../../packages/cspell-lsp { })
-        ]);
-      }
-      (lib.mkIf cfg.serverAliases {
-        home.shellAliases =
-          let
-            serverPipe = "\"\${HOME}/.config/nvim/server.pipe\"";
-          in
-          {
-            rvim = "nvim --server ${serverPipe}";
-            svim = "nvim --listen ${serverPipe}";
-          };
-      })
+    home.packages = [
+      wrappedNeovim
     ]
-  );
+    ++ (with pkgs; [
+      # For viewing images with Snacks.nvim
+      ffmpeg-full
+      ghostscript_headless
+      imagemagick
+      # For treesitter
+      tree-sitter
+      # TODO: add more configuration options for language servers
+      clang-tools
+      nixd
+      nixfmt-rfc-style
+      vscode-js-debug
+      vscode-langservers-extracted
+      (callPackage ../../packages/cspell-lsp { })
+    ]);
+  };
 }
