da08c865cb77a3b14d9ec5083680e6583af0621a
Jack Duvall <10897431+duvallj@users.noreply.github.com>
2026-03-12 21:10:46 -0400

    don't enable cspell all the time
    bun ipv6 is really pissing me off lol

--- nix/modules/home-manager/neovim.nix
+++ nix/modules/home-manager/neovim.nix
@@ -21,37 +21,49 @@ in
 {
   options.programs.neovim = {
     nonManagedEnable = lib.mkEnableOption "Whether to enable my custom non-managed Neovim configuration";
+
+    lsp = {
+      cspell = lib.mkEnableOption "Whether to enable building the cspell language server. Doesn't work if you have ipv6 lol";
+    };
   };
 
-  config = lib.mkIf cfg.nonManagedEnable {
-    # I don't use this because I want to track these dotfiles directly from git instead.
-    # programs.neovim.enable = true;
+  config = lib.mkIf cfg.nonManagedEnable (
+    lib.mkMerge [
+      {
+        # I don't use this because I want to track these dotfiles directly from git instead.
+        # programs.neovim.enable = true;
 
-    home.sessionVariables = {
-      EDITOR = "nvim";
-    };
+        home.sessionVariables = {
+          EDITOR = "nvim";
+        };
 
-    home.shellAliases = {
-      vimdiff = "nvim -d";
-    };
+        home.shellAliases = {
+          vimdiff = "nvim -d";
+        };
 
-    home.packages = [
-      wrappedNeovim
+        home.packages = [
+          wrappedNeovim
+        ]
+        ++ (with pkgs; [
+          # For viewing images with Snacks.nvim
+          ffmpeg-full
+          ghostscript_headless
+          imagemagick
+          # For treesitter
+          tree-sitter
+          # TODO: add more configuration options for language servers
+          clang-tools
+          nixd
+          nixfmt-rfc-style
+          vscode-js-debug
+          vscode-langservers-extracted
+        ]);
+      }
+      (lib.mkIf cfg.lsp.cspell {
+        home.packages = [
+          (pkgs.callPackage ../../packages/cspell-lsp { })
+        ];
+      })
     ]
-    ++ (with pkgs; [
-      # For viewing images with Snacks.nvim
-      ffmpeg-full
-      ghostscript_headless
-      imagemagick
-      # For treesitter
-      tree-sitter
-      # TODO: add more configuration options for language servers
-      clang-tools
-      nixd
-      nixfmt-rfc-style
-      vscode-js-debug
-      vscode-langservers-extracted
-      (callPackage ../../packages/cspell-lsp { })
-    ]);
-  };
+  );
 }
